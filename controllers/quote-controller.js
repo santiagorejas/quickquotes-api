707a9585735bc6e6973e02dbe096ecafaaf6cbe7
santiagorejas <santiago.orejasrg@gmail.com>
2022-07-04 00:07:18 -0300

    quote list with pagination added

--- controllers/quote-controller.js
+++ controllers/quote-controller.js
@@ -1,6 +1,9 @@
+const HttpError = require("../models/http-error");
 const Quote = require("../models/quote");
 
-const createQuote = (req, res, next) => {
+const QUOTES_PER_PAGE = 12;
+
+const createQuote = async (req, res, next) => {
     const { content } = req.body;
 
     const { nickname } = req.userData;
@@ -11,7 +14,36 @@ const createQuote = (req, res, next) => {
         date: new Date(),
     });
 
+    try {
+        await quote.save();
+    } catch (err) {
+        return next(new HttpError("Falló el almacenamiento de la quote.", 500));
+    }
+
     res.json(quote);
 };
 
+const getQuotes = async (req, res, next) => {
+    let { page } = req.query;
+
+    if (!page) {
+        page = 1;
+    }
+
+    let totalItems = 0;
+    let quotes = [];
+    try {
+        totalItems = await Quote.find().count();
+        quotes = await Quote.find()
+            .sort({ date: -1 })
+            .skip((page - 1) * QUOTES_PER_PAGE)
+            .limit(QUOTES_PER_PAGE);
+    } catch (err) {
+        return next(new HttpError("Falló la búsqueda de quotes.", 500));
+    }
+
+    res.json({ quotes, totalPages: Math.ceil(totalItems / QUOTES_PER_PAGE) });
+};
+
 exports.createQuote = createQuote;
+exports.getQuotes = getQuotes;
--- routes/quote-routes.js
+++ routes/quote-routes.js
@@ -6,4 +6,6 @@ const quoteController = require("../controllers/quote-controller");
 
 router.post("/", checkAuth, quoteController.createQuote);
 
+router.get("/", quoteController.getQuotes);
+
 module.exports = router;
